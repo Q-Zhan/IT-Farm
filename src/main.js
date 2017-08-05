db0a6abeea3501e9d2c89546d08cce73c54d2e79
Q-Zhan <380776767@qq.com>
2017-08-05 15:14:36 +0800

    引入淘宝flexible方案进行移动端适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import './utils/device' // 动态计算rem font-size
 
 // 使用axios进行数据请求
 Vue.use(VueAxios, axios)
