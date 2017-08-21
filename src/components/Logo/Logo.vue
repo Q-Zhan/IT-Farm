ce04e2a374635dafcc1c31dc13ce94f186d36c45
Q-Zhan <380776767@qq.com>
2017-08-21 14:40:47 +0800

    添加首页（提供注册和登录）

---
+++ src/components/Logo/Logo.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="logo">
+    <header>
+      <div class="logo_name">
+        <img src="./logo.png"/>
+        <span>匿密</span>
+      </div>
+      <div class="subhead">
+        华农人的<br/>匿名朋友圈
+      </div>
+    </header>
+    <div class="register">
+      <router-link to="/register">立即注册</router-link>
+    </div>
+    <div class="login">
+      <router-link to="/login">已有账号? 点此登录</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#logo {
+  width: 100%;
+  height: 100%;
+  background: url('./timg.jpg') no-repeat;
+  background-size: cover;
+  display: inline-block;
+  header {
+    margin-top: 3.5rem;
+    width: 100%;
+    .logo_name {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 1.3rem;
+      letter-spacing: 0.15rem;
+      color: white;
+      font-weight: 600;
+      img {
+        width: 1.3rem;
+        height: 1.3rem;
+        border-radius: 0.25rem;
+        margin-right: 0.5rem;
+      }
+    }
+    .subhead {
+      width: 100%;
+      margin-top: 0.2rem;
+      text-align: center;
+      color: white;
+      font-size: 0.55rem;
+      letter-spacing: 0.45rem;
+      line-height: 0.8rem;
+    }
+  }
+  .register {
+    width: 68%;
+    height: 1.1rem;
+    margin: 0 auto;
+    margin-top: 7.2rem;
+    line-height: 1.1rem;
+    font-size: 0.45rem;
+    letter-spacing: 1px;
+    text-align: center;
+    background: #007ACC;
+    border-radius: 0.12rem;
+    a {
+      color: white;
+    }
+  }
+  .login {
+    width: 100%;
+    margin-top: 0.5rem;
+    text-align: center;
+    font-size: 0.35rem;
+    letter-spacing: 2px;
+    a {
+      color: white;
+    }
+  }
+}
+</style>
