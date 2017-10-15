a4e48086d4aec4f7ae9427b864d24754761432e3
Q-Zhan <380776767@qq.com>
2017-10-15 22:33:06 +0800

    更改fetch请求接口地址（启用了nginx反向代理）

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
-export const api = 'http://104.224.174.146:8080/Mimi'
+// export const api = 'http://104.224.174.146:8080/Mimi'
+export const api = '/Mimi'
 export const api_scau = 'http://192.168.232.2:8080/Mimi'
 export const redirect_uri = 'http://123.207.18.102/hometownBind'
